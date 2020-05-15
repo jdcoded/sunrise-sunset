bd94e29d00d35bb91fbb5f158c8dd950bb03f6a1
Jeremy Dela Cruz <jeremy@jdcoded.com>
2020-05-15 14:31:25 -0700

    feat(sunrise-sunset): add some sexy hooks

---
+++ src/hooks/useSunData.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+import moment from "moment"
+
+export const useSunData = ({lat, lng, date}) => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [results, setResult] = useState([]);
+  const formattedDate = moment(date).format('YYYY-MM-DD')
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+
+    if (lat === 0 || lng === 0) {
+      return
+    }
+
+    fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=${formattedDate}&formatted=0`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          setIsLoaded(true);
+          setResult(result.results);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log('no results')
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [ lat, lng, formattedDate ])
+
+  return {
+    results, isLoaded, error
+  }
+}
