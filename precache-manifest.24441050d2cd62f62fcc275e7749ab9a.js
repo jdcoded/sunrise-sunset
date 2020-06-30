8caf968770b6aad94a4334e1f42a27710e147929
Jeremy Dela Cruz <jeremy@jdcoded.com>
2020-06-30 15:16:23 -0700

    Updates

---
+++ precache-manifest.24441050d2cd62f62fcc275e7749ab9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef120284f2d7578e01b3489e0d4eb539",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9383f738bd736092d9a1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4e23e5fa75136aeacfe1",
+    "url": "/static/js/2.e734053d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e734053d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9383f738bd736092d9a1",
+    "url": "/static/js/main.fbd28795.chunk.js"
+  },
+  {
+    "revision": "6426d2f4a2e4a825baf4",
+    "url": "/static/js/runtime-main.6991344e.js"
+  }
+]);
