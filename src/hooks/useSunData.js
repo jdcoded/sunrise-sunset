a3f573047e4883d3c7b553034b8c2a241dbe2e28
Jeremy Dela Cruz <jeremy@jdcoded.com>
2020-11-13 10:19:41 -0800

    feat(sunrise-sunset): add response caching to local storage

--- src/hooks/useSunData.js
+++ src/hooks/useSunData.js
@@ -3,19 +3,33 @@ import { useEffect, useState } from "react"
 export const useSunData = ({lat, lng, formattedDate}) => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [results, setResult] = useState([]);  
+  const [results, setResult] = useState({});  
 
   useEffect(() => {
 
     if (lat === 0 || lng === 0) {
+      console.log('invalid coords')
+      return
+    }
+
+    const sunriseCache = localStorage.getItem('sunriseResult')
+    
+    if (sunriseCache) {
+      console.log('cache hit')
+      setIsLoaded(true);
+      setResult(JSON.parse(sunriseCache).results);
       return
     }
 
     fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=${formattedDate}&formatted=0`)
       .then(res => res.json())
+      .then((res) => {
+        localStorage.setItem('sunriseResult', JSON.stringify(res))
+        return res
+      })
       .then(
         (result) => {
-          console.log(result)
+          // console.log(result)
           setIsLoaded(true);
           setResult(result.results);
         },
